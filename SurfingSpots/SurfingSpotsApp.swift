3c600ebfee319257a8afec177bad2e69b15eddd3
Gabriele Diletto <gabriele.diletto@gmail.com>
2021-12-10 14:15:03 +0100

    Initial Commit

---
+++ SurfingSpots/SurfingSpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfingSpotsApp.swift
+//  SurfingSpots
+//
+//  Created by Gabriele Diletto on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SurfingSpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
