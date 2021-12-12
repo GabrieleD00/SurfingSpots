6aac875ac604b6b8842d7cd264284dab263a8a85
Gabriele Diletto <gabriele.diletto@gmail.com>
2021-12-12 22:41:49 +0100

    Added enum WeatherConditions

---
+++ SurfingSpots/Infrastructure/WeatherConditions.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherConditions.swift
+//  SurfingSpots
+//
+//  Created by Gabriele Diletto on 12/12/21.
+//
+
+import Foundation
+
+enum WeatherConditions: String {
+    case sunny = "Sunny"
+    case cloudy = "Cloudy"
+}
