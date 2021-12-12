7b85204cf3340b9f55954bc03f05a63940a239c0
Gabriele Diletto <gabriele.diletto@gmail.com>
2021-12-12 22:40:23 +0100

    Added MainViewModel

---
+++ SurfingSpots/Features/Main/Presentation/MainViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  MainViewModel.swift
+//  SurfingSpots
+//
+//  Created by Gabriele Diletto on 12/12/21.
+//
+
+import Combine
+import SwiftUI
+import Foundation
+
+class MainViewModel: ObservableObject {
+    @Published var cities: [City] = []
+    
+    private let useCase: MainUseCaseInterface = MainUseCase()
+    private var disposables = Set<AnyCancellable>()
+    init() {
+        Task {
+            await useCase.getCities()
+                .receive(on: DispatchQueue.main)
+                .sink(receiveValue: { [weak self] newValues in
+                    guard let self = self else { return }
+                    
+                    self.cities = newValues
+                    
+                    withAnimation(.linear(duration: 0.5)) {
+                        self.cities.sort(by: { $0.temperature > $1.temperature })
+                    }
+                })
+                .store(in: &disposables)
+        }
+        
+        useCase.getUpdatedTemperature()
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] newTemperature in
+                guard let self = self else { return }
+                
+                self.updateRandomCity(newTemperature: newTemperature)
+            }
+            .store(in: &disposables)
+    }
+    
+    private func updateRandomCity(newTemperature: Int) {
+        if cities.isEmpty { return }
+        
+        let randomIndex = Int.random(in: cities.indices)
+        
+        cities[randomIndex].temperature = newTemperature
+        
+        withAnimation(.linear(duration: 0.5)) {
+            cities.sort(by: { $0.temperature > $1.temperature })
+        }
+    }
+    
+}
