b58dc68bc765a6ad130179ec7d0f55a286b7e6fe
Gabriele Diletto <gabriele.diletto@gmail.com>
2021-12-12 22:43:18 +0100

    Added logic to manage weather conditions

--- SurfingSpots/Infrastructure/City.swift
+++ SurfingSpots/Infrastructure/City.swift
@@ -5,6 +5,7 @@
 //  Created by Gabriele Diletto on 11/12/21.
 //
 
+import SwiftUI
 import Foundation
 
 struct City: Identifiable, Codable {
@@ -12,6 +13,18 @@ struct City: Identifiable, Codable {
     var name: String
     var temperature = Int.random(in: 0...60)
     
+    var weatherConditions: WeatherConditions {
+        if temperature >= 30 {
+            return .sunny
+        } else {
+            return .cloudy
+        }
+    }
+    
+    var image: Image {
+        return Image(name, bundle: .main)
+    }
+
     enum CodingKeys: CodingKey {
         case name
     }
